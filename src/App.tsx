4a2c9c4c341fc9a0a594b6333abb2f21f5ec10f9
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-21 17:03:41 -0400

    adds material ui to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,361 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <Box display='flex' flexDirection='column' height={1} width={1}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>Simple Web AR</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box flexGrow={1} overflow='hidden'>
+        <Box height={1} overflow='scroll'>
+          <Box display='flex' flexDirection='column' p={3}>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+            <Typography>Some content</Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
