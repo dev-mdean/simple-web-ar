be773e60422affc6aa5331f8bc3884f49699b00f
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-21 17:08:16 -0400

    fixes vite config issues to point to the new location of index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/simple-web-ar/',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [react()],
+  root: 'public',
   server: {
     port: 3000,
   },
