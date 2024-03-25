2dce893ad95bc4878b8fe4e6e60b51412f8e35fd
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-25 17:03:13 -0400

    removes non-working ar code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,76 +4,37 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
 import { isAndroid, isMobile, isMobileSafari } from 'react-device-detect'
-
-enum ViewingMode {
-  AR,
-  ThreeD,
-}
 
 interface Model {
   name: string
-  scaleAR: string
-  scaleThreeD: string
+  scale: string
 }
 
 const models = {
   robot: {
     name: 'robot-operator',
-    scaleAR: '0.25 0.25 0.25',
-    scaleThreeD: '1 1 1',
+    scale: '0.1 0.1 0.1',
   } as Model,
   cube: {
     name: 'gold-cube',
-    scaleAR: '0.1 0.1 0.1',
-    scaleThreeD: '1 1 1',
+    scale: '0.1 0.1 0.1',
   } as Model,
 }
 
 const App = () => {
   const modelViewerRef = useRef<ModelViewerElement>(null)
   const [model, setModel] = useState(models.cube)
-  const [viewingMode, setViewingMode] = useState(ViewingMode.ThreeD)
 
   const handleActivateAR = useCallback(() => {
     console.log('Activating AR')
     modelViewerRef.current?.activateAR()
-    setViewingMode(ViewingMode.AR)
   }, [])
 
   const handleExitAR = useCallback(() => {
     console.log('Exiting AR')
-    setViewingMode(ViewingMode.ThreeD)
   }, [])
-
-  const handleBeforeRender = useCallback(() => {
-    console.log('Before Render Called')
-    setViewingMode(ViewingMode.ThreeD)
-  }, [])
-
-  const handleQuickLookTapped = useCallback(() => {
-    console.log('Quick Look Tapped')
-    setViewingMode(ViewingMode.AR)
-  }, [])
-
-  const handleProgress = useCallback((event: unknown) => {
-    console.log('Progress Occurred: ', event)
-  }, [])
-
-  useEffect(() => {
-    const element = modelViewerRef.current
-
-    element?.addEventListener('before-render', handleBeforeRender)
-    element?.addEventListener('quick-look-button-tapped', handleQuickLookTapped)
-    element?.addEventListener('progress', handleProgress)
-
-    return () => {
-      element?.removeEventListener('before-render', handleBeforeRender)
-      element?.removeEventListener('quick-look-button-tapped', handleQuickLookTapped)
-      element?.removeEventListener('progress', handleProgress)
-    }
-  }, [handleBeforeRender, handleProgress, handleQuickLookTapped])
 
   const handleModelSwitch = useCallback(() => {
     setModel((previous) => {
@@ -86,11 +47,6 @@ const App = () => {
     if (model === models.robot) return models.cube
     return models.robot
   }, [model])
-
-  const currentScale = useMemo(
-    () => (viewingMode === ViewingMode.AR ? model.scaleAR : model.scaleThreeD),
-    [model.scaleAR, model.scaleThreeD, viewingMode],
-  )
 
   return (
     <Box display='flex' flexDirection='column' height={1} width={1}>
@@ -111,7 +67,7 @@ const App = () => {
               id='first'
               ios-src={`${model.name}.usdz`}
               ref={modelViewerRef}
-              scale={currentScale}
+              scale={model.scale}
               src={`${model.name}.glb`}
               style={{ backgroundColor: '#00000020', display: 'block', height: '50vh', width: '100%' }}
             >
